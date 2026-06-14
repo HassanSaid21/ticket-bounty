183f38ca3a8de03bd4b51df93febf5e1398b7e24
karim <hassansaid2528@gmail.com>
2026-06-14 19:43:55 +0300

    added theming

---
+++ src/components/theme/theme-switcher.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export default function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme === "light" ? <SunIcon /> : <MoonIcon />}
+    </Button>
+  );
+}
