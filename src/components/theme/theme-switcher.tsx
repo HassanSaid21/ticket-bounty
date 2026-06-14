a22825070e95b9680e478c689a1fe76cb63abdb6
karim <hassansaid2528@gmail.com>
2026-06-14 20:33:02 +0300

    fix hyderation issue

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -2,12 +2,17 @@
 
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
-
+  const [mount, setMount] = useState(false);
+  useEffect(() => {
+    setMount(true);
+  }, []);
+  if (!mount) return null;
   return (
     <Button
       variant="outline"
